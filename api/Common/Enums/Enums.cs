b029aee479d3b23adb41c276ff3b203dc452ae8b
Bojan Kuljić <116057046+BojanKuljic@users.noreply.github.com>
2024-06-07 20:54:22 +0200

    prepare enums

---
+++ api/Common/Enums/Enums.cs
@@ -0,0 +1,26 @@
+﻿namespace Common.Enums
+{
+    // Enumeracija koja predstavlja status verifikacije naloga
+    public enum StatusOfAccountVerification
+    {
+        Pending, // Nalog čeka na verifikaciju
+        Approved, // Nalog je verifikovan
+        Denied // Verifikacija naloga je odbijena
+    }
+
+    // Enumeracija koja predstavlja status vožnje
+    public enum StatusOfRide
+    {
+        InProgress, // Vožnja je u toku
+        Created, // Vožnja je kreirana
+        Done // Vožnja je završena
+    }
+
+    // Enumeracija koja predstavlja ulogu korisnika
+    public enum UserRole
+    {
+        Admin, // Administrator
+        User, // Običan korisnik
+        Driver // Vozač
+    }
+}
